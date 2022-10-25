43b0d261b0e3bcc2ee4f389a3ccd0f5e16d3e5f8
Luke Wainhouse <lukewainhouse@hotmail.com>
2022-10-25 17:08:07 +0100

    WW: DB configuration, sign-up config/login config, flash messages, home page access,

--- website/views.py
+++ website/views.py
@@ -1,24 +1,13 @@
 from flask import Blueprint, render_template, redirect, url_for
+from flask_login import login_required, current_user
 
 
 views = Blueprint("views", __name__)
 
 
+
 @views.route("/")
 @views.route("/home")
+@login_required
 def home():
-    return render_template("home.html" )
-
-@views.route("/logout")
-def logout():
-    return redirect(url_for("views.home"))
-
-
-@views.route("/login")
-def login():
-    return render_template("login.html")
-
-
-@views.route("/sign-up")
-def signup():
-    return render_template("signup.html")        
+    return render_template("home.html", name=current_user.username)
