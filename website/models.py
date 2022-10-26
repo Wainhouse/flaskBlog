043e62deb029c2c1b05984aaf6edfabb3af170c9
Luke Wainhouse <lukewainhouse@hotmail.com>
2022-10-26 17:05:03 +0100

    WW: Post creation functionality added,

--- website/models.py
+++ website/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
@@ -9,3 +10,11 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    posts = db.relationship('Post', backref='user', passive_deletes=True)
+
+
+class Post(db.Model):
+     id = db.Column(db.Integer, primary_key=True)
+     text = db.Column(db.Text, nullable=False)
+     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+     author = db.Colum(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE", nullable=false))
